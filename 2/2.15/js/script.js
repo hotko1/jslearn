ed91dc30461f8599124872eeed4e8efa9d3bcdb8
ar <a.honcharuk@internetdevels.com>
2023-03-15 18:09:08 +0200

    2.15v2

--- 2/2.15/js/script.js
+++ 2/2.15/js/script.js
@@ -19,3 +19,29 @@ console.log(4 + +'5');
 let incr = 10,
     decr = 10;
 
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+incr = 10;
+decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
+
+incr = 10;
+decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+incr = 10;
+decr = 10;
+
+console.log(++incr);
+console.log(--decr);
