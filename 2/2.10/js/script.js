32a9a6a6ffd6693d6a45369cabb678de233120fc
ar <a.honcharuk@internetdevels.com>
2023-03-15 14:26:51 +0200

    2-9

---
+++ 2/2.10/js/script.js
@@ -0,0 +1,32 @@
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
+
+console.log("Ширина кузова автомобіля: " + vehicleBodyWidth + ', довжина: ' + vehicleBodyLenght);
+
+
+// ['vruru', 'ecrvrev', 'vvuguegv'].map(a => 1);
+
+// Request
+// data
+// response
+
+
+// snake_case
+// UPPER_SNAKE_CASE
+const COLOR_RED = '#F00';
+
+
+// Kebab-case
+// PascalCase
+
+
+// camelCase
+
+const treeWidth = 10;
+
+
+// _camelCase
+
+const _apiBase = 'https://example.com/v1/public/';
+// Тут може бути ваш ключ, цей ключ може не працювати
+const _apiKey = 'apikey=gkgdflk4klgl4l';
---
+++ 2/2.9/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+let number = 5;
+let Number = 7;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+// leftBorderWidth = 10;
+// console.log(leftBorderWidth);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+
+console.log(name);;
+var name = 'Ivan';
+
+name = 'Alex';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+alert(5);
+[].push('a');
