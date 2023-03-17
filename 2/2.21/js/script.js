579640b39392d8d54054704a620643498586b52d
ar <a.honcharuk@internetdevels.com>
2023-03-17 12:22:51 +0200

    2.20v1

---
+++ 2/2.21/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const hamburger = true;
+const fries = true;
+
+if (hamburger && fries) {
+    console.log('Я ситий!');
+}
+
+console.log((hamburger && fries));
+
+
+const hamburgerr = 5;
+const friess = 0;
+
+if (hamburgerr && friess) {
+    console.log('Я ситий!');
+}
+
+console.log((hamburgerr && friess));
