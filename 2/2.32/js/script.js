43a334d510929fee2e3bdb889e6126f86b13738e
ar <a.honcharuk@internetdevels.com>
2023-06-23 10:19:25 +0300

    2.32v2

--- 2/2.32/js/script.js
+++ 2/2.32/js/script.js
@@ -9,8 +9,20 @@ const options = {
     colors: {
         border: 'black',
         bg: 'red',
+    },
+    makeTest: function() {
+        console.log('Test');
     }
 };
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+console.log(Object.keys(options));
+console.log(Object.keys(options).length);
+
 
 // console.log(options['colors']['border']);
 
@@ -21,12 +33,24 @@ const options = {
 
 // console.log(options);
 
+let counter = 0;
 for (let key in options) {
     if (typeof(options[key]) === 'object') {
         for (let i in options[key]) {
             console.log(`Властивість ${i} має значення ${options[key][i]}`);
+            // counter++;
         }
+        counter++;
     } else {
         console.log(`Властивість ${key} має значення ${options[key]}`);
+        counter++;
     }
 }
+console.log(counter);
+
+
+let counters = 0;
+for (let keys in options) {
+    counters++;
+}
+console.log(counters);
