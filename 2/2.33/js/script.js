f4499f6e1b49f1ccfd25a418cf05225b6b993021
ar <a.honcharuk@internetdevels.com>
2023-06-26 10:11:58 +0300

    2.33v2

--- 2/2.33/js/script.js
+++ 2/2.33/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const arr = [1, 2, 3, 6, 8];
+const arr = [1, 22, 13, 6, 8];
 
 console.log(arr.length);
 
@@ -13,10 +13,30 @@ for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
 
+// цей цикл зручний коли треба використовувати break чи continue.
 for (let value of arr) {
     console.log(value);
 }
 
-arr[99] = 0;
+// arr[99] = 0;
+// console.log(arr);
+// console.log(arr.length);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} всередині масива ${arr}`);
+});
+
+arr.sort();
 console.log(arr);
-console.log(arr.length);
+
+arr.sort(compareNum);
+console.log(arr);
+function compareNum(a, b) {
+    return a - b;
+}
+
+const str = prompt('', '');
+const products = str.split(', ');
+// console.log(products);
+products.sort();
+console.log(products.join('; '));
