1e13e251561df6e0081db85d8cdd8e41853ec2fc
ar <a.honcharuk@internetdevels.com>
2023-06-30 15:06:49 +0300

    2.35d10v2

--- 2/2.35d10/js/script.js
+++ 2/2.35d10/js/script.js
@@ -36,16 +36,67 @@ function showProgrammingLangs(plan) {
 showProgrammingLangs(personalPlanPeter);
 
 
-const showAgeAndLangs = {};
 let age,
     langs,
     string = '';
-personalPlanPeter.showAgeAndLangs = function(personalPlanPeter) {
-    langs = personalPlanPeter.skills.languages.toString().toUpperCase().replace(/,/, ' ');
-    age = personalPlanPeter.age;
+personalPlanPeter.showAgeAndLangs = function(plan) {
+    langs = plan.skills.languages.toString().toUpperCase().replace(/,/, ' ');
+    age = plan.age;
     string = `Мне ${age} и я владею языками: ${langs}`;
     console.log(string);
     return string;
 }
 
 personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+
+// Код ментора.
+const personalPlanPeters = {
+    name: 'Peter',
+    age: '29',
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+        },
+        exp: '1 month',
+    },
+    showAgeAndLangss: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+
+        console.log(str);
+        return str;
+    }
+}
+
+personalPlanPeters.showAgeAndLangss(personalPlanPeters);
+
+
+function showExperiences(plan) {
+    const {exp} = plan.skills;
+    console.log(exp);
+    return exp;
+}
+
+showExperiences(personalPlanPeters);
+
+
+function showProgrammingLangss(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+    }
+
+    console.log(str);
+    return str;
+}
+
+showProgrammingLangss(personalPlanPeters);
