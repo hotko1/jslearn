49a5217ee798e30296d1e34d02924841c0a8ba64
ar <a.honcharuk@internetdevels.com>
2023-07-05 17:52:38 +0300

    2.36

---
+++ 2/2.36/js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let str = 'some';
+let strObj = new String(str);
+
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+const jonhs = Object.create(soldier);
+
+const jonh = {
+    health: 100,
+};
+
+// jonh.__proto__ = soldier;
+
+Object.setPrototypeOf(jonh, soldier);
+
+console.log(jonh);
+console.log(jonh.armor);
+console.log(jonh.health);
+jonh.sayHello();
+jonhs.sayHello();
