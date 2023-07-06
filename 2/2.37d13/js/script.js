a26a33a3bf47a22c353fe6d45ca5a5e6f868bd5d
ar <a.honcharuk@internetdevels.com>
2023-07-06 14:15:56 +0300

    2.37d13v2

--- 2/2.37d13/js/script.js
+++ 2/2.37d13/js/script.js
@@ -17,20 +17,46 @@ const shoppingMallData = {
         {
             width: 8,
             length: 10
-        }
+        },
     ],
     height: 5,
     moneyPer1m3: 30,
-    budget: 50000
+    budget: 50000,
 }
 
 function isBudgetEnough(data) {
-    let square = 0;
-    for (let room in data.shops) {
-        square += data.shops.room.width * data.shops.room.length;
-        // for (let param in room) {
-        //     square += data.shops[0] * data.shops[1];
-        // } 
+    let volume,
+        square = 0;
+
+    // while (i < shoppingMallData.shops.length) {
+    //     let multipl = 1;
+    //     for (let param in shoppingMallData.shops[i]) {
+    //         multipl *= data.shops[i][param];
+    //     }
+    //     square += multipl;
+    //     i++;
+    // }
+    for (let i = 0; i < data.shops.length; i++) {
+        let multipl = 1;
+        for (let param in data.shops[i]) {
+            multipl *= data.shops[i][param];
+        }
+        square += multipl;
     }
+
+    volume = square * data.height;
+
     console.log(square);
+    console.log(volume);
+    console.log(data.budget);
+
+    if (volume <= data.budget) {
+        console.log('Бюджета достаточно');
+        return 'Бюджета достаточно';
+    } else {
+        console.log('Бюджета недостаточно');
+        return 'Бюджета недостаточно';
+    }
 }
+
+isBudgetEnough(shoppingMallData);
