89f4adbd757759637459b6ba4d5e36dff51b0b82
ar <a.honcharuk@internetdevels.com>
2023-07-07 15:48:03 +0300

    2.37d14v2

--- 2/2.37d14/js/script.js
+++ 2/2.37d14/js/script.js
@@ -2,6 +2,7 @@
 
 const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
+// Мій код.
 function sortStudentsByGroups(arr) {
     let sortStudents = arr.sort();
     let newMas = [];
@@ -30,3 +31,25 @@ function sortStudentsByGroups(arr) {
 }
 
 sortStudentsByGroups(students);
+
+// Код ментора.
+function sortStudentsByGroupss(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i <arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    console.log([a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]);
+    return [a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+
+sortStudentsByGroupss(students);
