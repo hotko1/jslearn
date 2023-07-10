9b8ce5c1e513ee6353a4085531185d4fe16fcc5d
ar <a.honcharuk@internetdevels.com>
2023-07-10 15:23:23 +0300

    2.40v3

--- 2/2.40/js/script.js
+++ 2/2.40/js/script.js
@@ -18,17 +18,31 @@
 function createCounter() {
     let counter = 0;
 
-    const myFunction = function() {
-        counter = counter + 1;
-        return counter;
+    const myFunction = function() { debugger
+        counter = counter + 1; debugger
+        return counter; debugger
     }
 
     return myFunction;
 }
-
-const increment = createCounter();
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
+debugger
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
 
 console.log(c1, c2, c3);
+
+{
+    let msg = 'hello';
+}
+
+console.log(msg);
+
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        let num = 3;
+    }
+
+    console.log(num);
+}
