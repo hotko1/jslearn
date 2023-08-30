2625b2a204eba6b916afb3c92327f86f6985a586
ar <a.honcharuk@internetdevels.com>
2023-08-30 17:07:52 +0300

    2.45v2

--- 2/2.45/js/script.js
+++ 2/2.45/js/script.js
@@ -1,28 +1,63 @@
 'use strict';
 
-const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
-// btn.onclick = function() {
+// btns.onclick = function() {
 //     alert('Click');
 // };
 
-// btn.onclick = function() {
+// btns.onclick = function() {
 //     alert('Second click');
 // };
 
-btn.addEventListener('click', () => {
-    alert('Click');
-});
+// btns.addEventListener('click', () => {
+//     alert('Click');
+// });
 
-btn.addEventListener('click', () => {
-    alert('Second click');
-});
+// btns.addEventListener('click', () => {
+//     alert('Second click');
+// });
 
-// btn.addEventListener('mouseenter', () => {
+// btns.addEventListener('mouseenter', () => {
 //     console.log('Hover');
 // });
 
-btn.addEventListener('mouseenter', (e) => {
-    console.log(e.target);
-    e.target.remove();
+// btns.addEventListener('mouseenter', (e) => {
+//     console.log(e.target);
+//     e.target.remove();
+// });
+
+// btns.addEventListener('click', (e) => {
+//     // console.log(e.target);
+//     e.target.remove();
+// });
+
+// let i = 0;
+const deleteElement = (e) => {
+    // e.target.remove();
+    // console.log(e.target);
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if (i == 1) {
+    //     btns.removeEventListener('click', deleteElement);
+    // }
+}
+
+// btns.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+// btns.removeEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    console.log(event.target);
 });
