fb35d1476bbef3630d8cb5a11e58405720fb5951
ar <a.honcharuk@internetdevels.com>
2023-09-25 10:20:08 +0300

    2.48v9

--- 2/2.48/js/script.js
+++ 2/2.48/js/script.js
@@ -19,6 +19,8 @@ P.S. Здесь есть несколько вариантов решения з
 
 // Возьмите свой код из предыдущей практики
 
+// Мій код
+/*
 let btnFilm = document.querySelector('button');
 let inputFilm = document.querySelector('.adding__input');
 const movieDB = {
@@ -83,3 +85,46 @@ function checkCheckbox () {
         console.log('Додаємо улюблений фільм');
     }
 }
+*/
+
+
+// Код ментора
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list');
+    
+    adv.forEach(item => {
+        item.remove();
+    });
+    
+    genre.textContent = 'Драма';
+    
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+    
+    movieList.innerHTML = '';
+    
+    movieDB.movies.sort();
+    
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+
+});
