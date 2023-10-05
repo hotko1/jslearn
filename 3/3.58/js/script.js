ccf88e01ddfb997c35ff37acdbb22765b4cdfb95
ar <a.honcharuk@internetdevels.com>
2023-10-05 17:19:59 +0300

    3.58v2

--- 3/3.58/js/script.js
+++ 3/3.58/js/script.js
@@ -9,18 +9,37 @@ const user = {
     }
 }
 
+// for (const key of user) {
+//     console.log(user[key]);
+// }
+
 for (const key in user) {
     console.log(user[key]);
 }
 
+
 const arr = ['b', 'a', 'c'];
+Array.prototype.someMethod = function() {};
+
+console.dir(arr);
+
+for (const key in arr) {
+    console.log(key);
+}
 
 for (const key in arr) {
     console.log(arr[key]);
 }
+
+for (const key of arr) {
+    console.log(key);
+}
+
 
 const str = 'string';
 
 for (const key in str) {
     console.log(str[key]);
 }
+
+// for in - не завжди перебирає властивості не обов'язково по порядку і тому не рекомендується його застосовувати на масивах і рядках (string).
