78a14fea880d2e1b8c6bec20647fd8b3c897cde9
ar <a.honcharuk@internetdevels.com>
2023-10-06 12:12:11 +0300

    Some testv2

--- test/1/js/scritp.js
+++ test/1/js/scritp.js
@@ -1,2 +1,26 @@
 'use strict';
 
+addEventListener('resize', (e) => {
+    const heightArea = document.querySelector('.area').clientHeight;
+    console.log(heightArea);
+    const widthArea = document.querySelector('.area').clientWidth;
+    console.log(widthArea);
+
+    headFunction();
+});
+
+const heightArea = document.querySelector('.area').clientHeight;
+console.log(heightArea);
+const widthArea = document.querySelector('.area').clientWidth;
+console.log(widthArea);
+
+function headFunction () {
+    for (let i = 0; i < heightArea; i++) {
+        for (let k = 0; k < widthArea; k++) {
+            const el = document.querySelector('.area').appendChild('span');
+            el.classList.add(`${heightArea}-${widthArea}`);
+        }
+    }
+}
+
+headFunction();
