42eca0c8a2753743e1160eec659bda9c5d792151
ar <a.honcharuk@internetdevels.com>
2023-10-27 14:21:08 +0300

    3.61d17v4

--- 3/3.61d17/js/script.js
+++ 3/3.61d17/js/script.js
@@ -1,20 +1,41 @@
 'use strict';
 
-function amountOfPages(summary) {
-    if (summary >= 1) {
-        let i = '',
-        sum = '';
-        while (sum.length < summary) {
-            sum += `${i}`;
-            i++;
-        }
+// Мій код
+// function amountOfPages(summary) {
+//     if (summary >= 1) {
+//         let i = '',
+//         sum = '';
+//         while (sum.length < summary) {
+//             sum += `${i}`;
+//             i++;
+//         }
 
-        console.log(i - 1);
-        return (i - 1);
-    } else {
-        console.log(0);
-        return 0;
+//         console.log(i - 1);
+//         return (i - 1);
+//     } else {
+//         console.log(0);
+//         return 0;
+//     }
+// }
+
+// amountOfPages(11);
+
+
+// Код ментора (є вірним)
+function amountOfPages(summary) {
+    let result = '';
+    let n = 0;
+
+    for (let i = 1; i <= summary; i++) {
+        result += i;
+        if (result.length === summary) {
+            n = i;
+            break;
+        }
     }
+
+    console.log(n);
+    return n;
 }
 
-amountOfPages(11);
+amountOfPages(25);
